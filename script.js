aa03bdc17e9fc2b7ac44152ad78d7cf84310622f
valiensradix <121977172+valiensradix@users.noreply.github.com>
2024-06-24 11:41:00 -0400

    added div, order changed
    moved order, may have to add id to second (new) div.

--- script.js
+++ script.js
@@ -35,11 +35,6 @@ function displayFlashcard(index) {
     }
 }
 
-// show answers
-function showAnswers(index) {
-    alert(flashcards[index].answers.join(', '));
-}
-
 // check user answer
 function checkAnswer() {
     const userAnswer = document.getElementById('user-answer').value.toLowerCase();
@@ -50,6 +45,11 @@ function checkAnswer() {
     } else {
         alert('The answers are: ' + flashcards[currentIndex].answers.join(', '));
     }
+
+// show answers
+function showAnswers(index) {
+    alert(flashcards[index].answers.join(', '));
+}
 
     // Clear input field
     document.getElementById('user-answer').value = '';
